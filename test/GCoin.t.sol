792da121fcd14f4a4e683f95b39e828723847a6a
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-02-17 14:46:56 +0530

    error commit

--- test/GCoin.t.sol
+++ test/GCoin.t.sol
@@ -20,31 +20,46 @@ contract TestGCoin is Test {
         assertEq(c.balanceOf(address(this)), 100, "ok");
 
         // checks balance of other random address
-        assertEq(c.balanceOf(0x904025Cfcbe81947A92501D0857F0E33267a2b4F), uint256(0), "ok");
+        assertEq(
+            c.balanceOf(0x904025Cfcbe81947A92501D0857F0E33267a2b4F),
+            uint256(0),
+            "ok"
+        );
 
         // mints to annew address
         c.mint(0x904025Cfcbe81947A92501D0857F0E33267a2b4F, 100);
-        assertEq(c.balanceOf(0x904025Cfcbe81947A92501D0857F0E33267a2b4F), 100, "ok");
-
+        assertEq(
+            c.balanceOf(0x904025Cfcbe81947A92501D0857F0E33267a2b4F),
+            100,
+            "ok"
+        );
     }
 
     function testTransfer() public {
         //  mint to myself
-        c.mint(address(this), 100); 
+        c.mint(address(this), 100);
         // mint to a diff address half the money
-        c.transfer(0x904025Cfcbe81947A92501D0857F0E33267a2b4F, 50); 
+        c.transfer(0x904025Cfcbe81947A92501D0857F0E33267a2b4F, 50);
 
         // check if I have half left
-        assertEq(c.balanceOf(address(this)), 50); 
+        assertEq(c.balanceOf(address(this)), 50);
         // check if the other person reciveid the half
-        assertEq(c.balanceOf(0x904025Cfcbe81947A92501D0857F0E33267a2b4F), 50, "ok"); 
+        assertEq(
+            c.balanceOf(0x904025Cfcbe81947A92501D0857F0E33267a2b4F),
+            50,
+            "ok"
+        );
 
         // changes who is calling this contract
         vm.prank(0x904025Cfcbe81947A92501D0857F0E33267a2b4F);
         // retusn the moeny to the first sender
         c.transfer(address(this), 25);
         // checks if the 2nd sender has half the money
-        assertEq(c.balanceOf(0x904025Cfcbe81947A92501D0857F0E33267a2b4F), 25, "ok");
+        assertEq(
+            c.balanceOf(0x904025Cfcbe81947A92501D0857F0E33267a2b4F),
+            25,
+            "ok"
+        );
         // checks if the 1nd sender has rest of the money
         assertEq(c.balanceOf(address(this)), 75, "ok");
     }
@@ -56,21 +71,55 @@ contract TestGCoin is Test {
         c.approve(0x904025Cfcbe81947A92501D0857F0E33267a2b4F, 50);
 
         // check if they are allowed from my address to acess 50 tokens
-        assertEq(c.allowance(address(this), 0x904025Cfcbe81947A92501D0857F0E33267a2b4F), 50);
+        assertEq(
+            c.allowance(
+                address(this),
+                0x904025Cfcbe81947A92501D0857F0E33267a2b4F
+            ),
+            50
+        );
         // check if I am allowed to access tokens from their address
-        assertEq(c.allowance(0x904025Cfcbe81947A92501D0857F0E33267a2b4F ,address(this)), 0);
+        assertEq(
+            c.allowance(
+                0x904025Cfcbe81947A92501D0857F0E33267a2b4F,
+                address(this)
+            ),
+            0
+        );
 
         // change the address
-        vm.prank(0x904025Cfcbe81947A92501D0857F0E33267a2b4F);   
+        vm.prank(0x904025Cfcbe81947A92501D0857F0E33267a2b4F);
         // 2nd address trying to transfer to themselves (for now, this can be other address)
         // 25 tokens from my address
-        c.transferFrom(address(this), 0x904025Cfcbe81947A92501D0857F0E33267a2b4F, 25);
+        c.transferFrom(
+            address(this),
+            0x904025Cfcbe81947A92501D0857F0E33267a2b4F,
+            25
+        );
 
         // check my balance
         assertEq(c.balanceOf(address(this)), 75, "ok");
         // check balance of 2nd address
-        assertEq(c.balanceOf(address(0x904025Cfcbe81947A92501D0857F0E33267a2b4F)), 25, "ok");
+        assertEq(
+            c.balanceOf(address(0x904025Cfcbe81947A92501D0857F0E33267a2b4F)),
+            25,
+            "ok"
+        );
         // check how much allowence is lfet for them
-        assertEq(c.allowance(address(this), 0x904025Cfcbe81947A92501D0857F0E33267a2b4F), 25);
+        assertEq(
+            c.allowance(
+                address(this),
+                0x904025Cfcbe81947A92501D0857F0E33267a2b4F
+            ),
+            25
+        );
+    }
+
+    function test_Revert_Approvals() public {
+        c.mint(address(this), 100);
+        c.approve(0x587EFaEe4f308aB2795ca35A27Dff8c1dfAF9e3f, 10);
+        
+        vm.prank(0x587EFaEe4f308aB2795ca35A27Dff8c1dfAF9e3f);
+        c.transferFrom(address(this), 0x587EFaEe4f308aB2795ca35A27Dff8c1dfAF9e3f, 100);
     }
 }
