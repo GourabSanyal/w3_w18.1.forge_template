c443bf5bd9e17275828953fef5b092bbd021510c
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-02-17 12:42:49 +0530

    transfer test add

--- test/GCoin.t.sol
+++ test/GCoin.t.sol
@@ -27,4 +27,25 @@ contract TestGCoin is Test {
         assertEq(c.balanceOf(0x904025Cfcbe81947A92501D0857F0E33267a2b4F), 100, "ok");
 
     }
+
+    function testTransfer() public {
+        //  mint to myself
+        c.mint(address(this), 100); 
+        // mint to a diff address half the money
+        c.transfer(0x904025Cfcbe81947A92501D0857F0E33267a2b4F, 50); 
+
+        // check if I have half left
+        assertEq(c.balanceOf(address(this)), 50); 
+        // check if the other person reciveid the half
+        assertEq(c.balanceOf(0x904025Cfcbe81947A92501D0857F0E33267a2b4F), 50, "ok"); 
+
+        // changes who is calling this contract
+        vm.prank(0x904025Cfcbe81947A92501D0857F0E33267a2b4F);
+        // retusn the moeny to the first sender
+        c.transfer(address(this), 25);
+        // checks if the 2nd sender has half the money
+        assertEq(c.balanceOf(0x904025Cfcbe81947A92501D0857F0E33267a2b4F), 25, "ok");
+        // checks if the 1nd sender has rest of the money
+        assertEq(c.balanceOf(address(this)), 75, "ok");
+    }
 }
